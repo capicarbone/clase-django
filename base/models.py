0fb0c4c4c006cd1f0bbae4736675d124d532d62b
Carlos <cpinelly@gmail.com>
2012-11-11 22:33:44 -0430

    Primera subida

---
+++ base/models.py
@@ -0,0 +1,33 @@
+#encoding:utf-8
+
+from django.db import models
+from django.db.models import Model
+
+class Seccion(Model):
+
+	PROFESORES = (
+		( 'AL', 'Andrés Lillo' ),
+		( 'OS' , 'Oscar Salazar' )
+	)
+
+	MATERIAS = (
+		('PII' , 'Programación II'),
+		('EDT' , 'Estructura de datos'),
+		('PRI' , 'Programación I')
+	)
+
+	numero = models.IntegerField()
+	materia = models.CharField(max_length=3, choices=MATERIAS)
+	profesor = models.CharField(max_length=2, choices=PROFESORES)
+
+	class Meta:
+		verbosa_name_plural = 'Profesores'
+
+
+class Alumno(Model):
+
+	nombre = models.CharField(max_length=30, help_text='Nombre del alumno')
+	apellido = models.CharField(max_length=30, help_text='Apellido del alumno')
+	cedula = models.CharField(max_length=9, help_text='Comienze con tipo de cédula')
+	seccion = models.ForeignKey("Seccion")
+
